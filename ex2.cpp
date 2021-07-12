86a960c59f0ab2baa7b7021e2ec4906c2b7cac63
Jubirao <gabriels1850@gmail.com>
2021-07-12 18:23:14 -0300

    Exercícios em C

---
+++ ex2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+	
+	int minhaIdade;
+	minhaIdade = 18; 
+	int maeIdade = 48;
+	int paiIdade = 49;
+	
+	printf ("Minha idade: %i.\nPai idade: %i\nMae idade = %i\n",
+	 minhaIdade, paiIdade, maeIdade);
+	return 0;
+}
---
+++ ex3.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+	
+	int minhaIdade = 18;
+	int maeIdade = 46;
+	int paiIdade = 50;
+	int irmaoIdade = 15;
+	int idadeTotal;
+	
+	idadeTotal = minhaIdade + maeIdade + paiIdade + irmaoIdade;
+	
+	printf("%i\n", idadeTotal);
+}
